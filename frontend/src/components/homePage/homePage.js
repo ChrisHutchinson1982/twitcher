dbf5f1c12466c7da8c5c3e732161d4f2cd928803
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-06 22:37:01 +0000

    Added CCS

--- frontend/src/components/homePage/homePage.js
+++ frontend/src/components/homePage/homePage.js
@@ -3,12 +3,6 @@ import SearchBar from "../searchBar/searchBar";
 const HomePage = () => {
   return (
     <main id="main-container">
-      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-        <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-        <p className="text-gray-500 text-lg">
-          React and Tailwind CSS in action
-        </p>
-      </div>
       <div>
         <SearchBar />
       </div>
--- frontend/src/components/searchBar/searchBar.js
+++ frontend/src/components/searchBar/searchBar.js
@@ -19,18 +19,35 @@ const SearchBar = () => {
   };
 
   return (
-    <div id="searchBar">
-      <h1 data-cy="searchBarLabel">What animal have you spotted?</h1>
+    <div className="sm:container sm:mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <form onSubmit={handleSubmit}>
-        <input
-          data-cy="animalName"
-          type="text"
-          placeholder="e.g. Robin"
-          required
-          value={animal}
-          onChange={handleChange}
-        />
-        <input data-cy="searchSubmit" type="submit" value="Submit" />
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span
+              className="label-text text-gray-700 font-bold mb-1"
+              data-cy="searchBarLabel"
+            >
+              What animal have you spotted?
+            </span>
+          </label>
+          <input
+            className="input input-bordered input-s"
+            data-cy="animalName"
+            type="text"
+            placeholder="e.g. Robin"
+            required
+            value={animal}
+            onChange={handleChange}
+          />
+          <div className="pt-2">
+            <input
+              className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg w-full max-w-xs"
+              data-cy="searchSubmit"
+              type="submit"
+              value="Submit"
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
