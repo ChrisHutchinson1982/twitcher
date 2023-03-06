4ad7d86704bbe565c1e547f0b3e3e074e17485c8
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-06 21:53:02 +0000

    Passed test user can type animal name, click submit and GET request sent

--- frontend/src/components/searchBar/searchBar.cy.js
+++ frontend/src/components/searchBar/searchBar.cy.js
@@ -1,8 +1,11 @@
 import SearchBar from "./searchBar";
 
 describe("SearchBar", () => {
-  it("has field to enter animal name and submit buttom", () => {
+  beforeEach(() => {
     cy.mount(<SearchBar />);
+  });
+
+  it("has field to enter animal name and submit buttom", () => {
     cy.get('[data-cy="searchBarLabel"]').should(
       "contain.text",
       "What animal have you spotted?"
@@ -11,5 +14,14 @@ describe("SearchBar", () => {
     cy.get('[data-cy="searchSubmit"]')
       .invoke("attr", "type")
       .should("eq", "submit");
+  });
+
+  it("user can type animal and click on submit button", () => {
+    cy.intercept("GET", "/animals?name=cheetah").as("searchRequest");
+
+    cy.get('[data-cy="animalName"]').type("cheetah");
+    cy.get('[data-cy="searchSubmit"]').click();
+
+    cy.wait("@searchRequest");
   });
 });
--- frontend/src/components/searchBar/searchBar.js
+++ frontend/src/components/searchBar/searchBar.js
@@ -1,16 +1,35 @@
+import React, { useState } from "react";
+
 const SearchBar = () => {
+  const [animal, setAnimal] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await fetch(`http://localhost:8080/animals?name=${animal}`).then(
+      (response) => {
+        return response.json();
+      }
+    );
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    return setAnimal(event.target.value);
+  };
+
   return (
     <div id="searchBar">
       <h1 data-cy="searchBarLabel">What animal have you spotted?</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           data-cy="animalName"
           type="text"
           placeholder="e.g. Robin"
-          // value={animal}
-          // onChange={handleChange(setAnimal)}
+          required
+          value={animal}
+          onChange={handleChange}
         />
-
         <input data-cy="searchSubmit" type="submit" value="Submit" />
       </form>
     </div>
