c6796a517584fece528dff01c258868068cd4ef5
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-06 16:02:05 +0000

    Set up searchBar component files ready to start TDD

--- frontend/src/components/homePage/homePage.js
+++ frontend/src/components/homePage/homePage.js
@@ -1,10 +1,15 @@
-function App() {
+import SearchBar from "../searchBar/searchBar";
+
+const HomePage = () => {
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
       <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
+      <div>
+        <SearchBar />
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default HomePage;
