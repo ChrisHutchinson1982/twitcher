811e87dc531ad60e19a80701f689809f9f6dfca3
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-07 21:32:31 +0000

    Fixed key error on searchResults

--- frontend/src/components/searchBar/searchBar.js
+++ frontend/src/components/searchBar/searchBar.js
@@ -26,42 +26,44 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="w-1/3 pl-20">
-      <div className="sm:container sm:mx-auto bg-gray-200 rounded-xl shadow border p-4 m-10">
-        <form
-          className="form-control w-full max-w-xs space-y-2"
-          onSubmit={handleSubmit}
-        >
-          <label className="label">
-            <span
-              className="label-text text-gray-700 font-bold text-base"
-              data-cy="searchBarLabel"
-            >
-              What animal have you spotted?
-            </span>
-          </label>
-          <input
-            className="input input-bordered input-s"
-            data-cy="animalName"
-            type="text"
-            placeholder="e.g. Robin"
-            required
-            value={animal}
-            onChange={handleChange}
-          />
-          <input
-            className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-            data-cy="searchSubmit"
-            type="submit"
-            value="Submit"
-          />
-          <SearchResults
-            animalSearch={animalSearch}
-            renderSearch={renderSearch}
-          />
-        </form>
+    <>
+      <div className="w-1/3 pl-20">
+        <div className="sm:container sm:mx-auto bg-gray-200 rounded-xl shadow border p-4 m-10">
+          <form
+            className="form-control w-full max-w-xs space-y-2"
+            onSubmit={handleSubmit}
+          >
+            <label className="label">
+              <span
+                className="label-text text-gray-700 font-bold text-base"
+                data-cy="searchBarLabel"
+              >
+                What animal have you spotted?
+              </span>
+            </label>
+            <input
+              className="input input-bordered input-s"
+              data-cy="animalName"
+              type="text"
+              placeholder="e.g. Robin"
+              required
+              value={animal}
+              onChange={handleChange}
+            />
+            <input
+              className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+              data-cy="searchSubmit"
+              type="submit"
+              value="Submit"
+            />
+            <SearchResults
+              animalSearch={animalSearch}
+              renderSearch={renderSearch}
+            />
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/searchResults/searchResults.js
+++ frontend/src/components/searchResults/searchResults.js
@@ -2,13 +2,13 @@ const SearchResults = ({ animalSearch, renderSearch }) => {
   if (renderSearch) {
     return (
       <>
-        {animalSearch.map((animal) => {
+        {animalSearch.map((animal, index) => {
           return (
-            <div data-cy={`animalNameResult${animal.name}`}>
+            <div data-cy={`animalNameResult${animal.name}`} key={index}>
               <div>{animal.name}</div>
               <div>Where do they live?</div>
-              {animal.locations.map((place) => {
-                return <div>{place}</div>;
+              {animal.locations.map((place, index) => {
+                return <div key={index}>{place}</div>;
               })}
               <div>What do the eat?</div>
               <div>{animal.characteristics.prey}</div>
