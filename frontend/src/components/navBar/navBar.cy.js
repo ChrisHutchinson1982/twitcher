28e399f17417ae0d3b53bafa0209e6d06aef4a3e
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-08 21:09:50 +0000

    Added CSS to navBar and back ground image

--- frontend/src/components/navBar/navBar.cy.js
+++ frontend/src/components/navBar/navBar.cy.js
@@ -3,6 +3,6 @@ import NavBar from "./navBar";
 describe("NavBar", () => {
   it("Renders NavBar component with header", () => {
     cy.mount(<NavBar />);
-    cy.get('[data-cy="siteHeader"]').should("contain.text", "Spotted");
+    cy.get('[data-cy="siteHeader"]').should("contain.text", "Twitcher");
   });
 });
--- frontend/src/components/navBar/navBar.js
+++ frontend/src/components/navBar/navBar.js
@@ -1,8 +1,8 @@
 const NavBar = () => {
   return (
-    <div className="navbar bg-sky-300 text-primary-content">
+    <div className="navbar bg-gradient-to-r  from-black text-white">
       <a className="btn btn-ghost normal-bold text-5xl" data-cy="siteHeader">
-        Spotted
+        Twitcher
       </a>
     </div>
   );
