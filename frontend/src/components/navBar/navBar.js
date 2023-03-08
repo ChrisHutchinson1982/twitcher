8063d03617dbad46eed1793430f3cd77788c1880
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-08 22:07:33 +0000

    Added react icon to navBar

--- frontend/src/components/navBar/navBar.js
+++ frontend/src/components/navBar/navBar.js
@@ -1,7 +1,10 @@
+import { GiHummingbird } from "react-icons/gi";
+
 const NavBar = () => {
   return (
     <div className="navbar bg-gradient-to-r  from-black text-white">
       <a className="btn btn-ghost normal-bold text-5xl" data-cy="siteHeader">
+        <GiHummingbird />
         Twitcher
       </a>
     </div>
