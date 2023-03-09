20ff80a5802b25ce8a603d0ce7d731ed70444a42
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-09 16:10:42 +0000

    Passed e2e that user search renders mutiple search results with animal name on the page

--- frontend/cypress/e2e/user_search.cy.js
+++ frontend/cypress/e2e/user_search.cy.js
@@ -1,6 +1,6 @@
 import mockSearchResults from "../../src/components/searchResults/mockSearchResults";
 
-describe("", () => {
+describe("User animal search", () => {
   beforeEach(() => {
     cy.intercept("GET", "http://localhost:8080/animals?name=Robin", (req) => {
       req.reply({
@@ -10,7 +10,7 @@ describe("", () => {
     }).as("getAnimals");
   });
 
-  it("user sucessfully completes a search and renders one search result on the page", () => {
+  it("user sucessfully completes a search and renders one search result with animal name on the page", () => {
     cy.visit("/");
 
     cy.get('[data-cy="animalName"]').type("Robin");
@@ -20,6 +20,28 @@ describe("", () => {
       cy.get('[data-cy="animalNameResultAmerican Robin"]').should(
         "contain.text",
         "American Robin"
+      );
+    });
+  });
+
+  it("user sucessfully completes a search and renders all search results with animal name on the page", () => {
+    cy.visit("/");
+
+    cy.get('[data-cy="animalName"]').type("Robin");
+    cy.get('[data-cy="searchSubmit"]').click();
+
+    cy.wait("@getAnimals").then(() => {
+      cy.get('[data-cy="animalNameResultAmerican Robin"]').should(
+        "contain.text",
+        "American Robin"
+      );
+      cy.get('[data-cy="animalNameResultEuropean Robin"]').should(
+        "contain.text",
+        "European Robin"
+      );
+      cy.get('[data-cy="animalNameResultRobin"]').should(
+        "contain.text",
+        "Robin"
       );
     });
   });
