3571bf3407cdb75ac65ac05f9d75361e335479bf
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-13 10:19:04 +0000

    Refactored searchAnimal into separate component and passed test to render animal details

---
+++ frontend/src/components/searchAnimal/searchAnimal.js
@@ -0,0 +1,32 @@
+const SearchAnimals = ({ animal, index }) => {
+  return (
+    <div
+      className="sm:container sm:mx-auto bg-white rounded-xl shadow border p-4"
+      data-cy={`animalNameResult${animal.name}`}
+      key={index}
+    >
+      <h1 className="label-text text-black font-bold text-2xl pb-2">
+        {animal.name}
+      </h1>
+      <h2 className="label-text text-black font-bold text-sm">
+        Where do they live?
+      </h2>
+      {animal.locations.map((place, index) => {
+        return (
+          <p className="label-text text-black text-sm" key={index}>
+            {place}
+          </p>
+        );
+      })}
+      <h2 className="label-text text-black font-bold text-sm pt-2">
+        What do they eat?
+      </h2>
+      <p className="label-text text-black text-sm pb-2">
+        {animal.characteristics.prey}
+        {animal.characteristics.main_prey}
+      </p>
+    </div>
+  );
+};
+
+export default SearchAnimals;
--- frontend/src/components/searchResults/searchResults.js
+++ frontend/src/components/searchResults/searchResults.js
@@ -1,4 +1,5 @@
 import { GiHummingbird } from "react-icons/gi";
+import SearchAnimal from "../searchAnimal/searchAnimal";
 
 const SearchResults = ({ animalSearch, renderSearch, loading }) => {
   if (loading) {
@@ -17,32 +18,33 @@ const SearchResults = ({ animalSearch, renderSearch, loading }) => {
       <>
         {animalSearch.map((animal, index) => {
           return (
-            <div
-              className="sm:container sm:mx-auto bg-white rounded-xl shadow border p-4"
-              data-cy={`animalNameResult${animal.name}`}
-              key={index}
-            >
-              <h1 className="label-text text-black font-bold text-2xl pb-2">
-                {animal.name}
-              </h1>
-              <h2 className="label-text text-black font-bold text-sm">
-                Where do they live?
-              </h2>
-              {animal.locations.map((place, index) => {
-                return (
-                  <p className="label-text text-black text-sm" key={index}>
-                    {place}
-                  </p>
-                );
-              })}
-              <h2 className="label-text text-black font-bold text-sm pt-2">
-                What do they eat?
-              </h2>
-              <p className="label-text text-black text-sm pb-2">
-                {animal.characteristics.prey}
-                {animal.characteristics.main_prey}
-              </p>
-            </div>
+            <SearchAnimal animal={animal} index={index} />
+            // <div
+            //   className="sm:container sm:mx-auto bg-white rounded-xl shadow border p-4"
+            //   data-cy={`animalNameResult${animal.name}`}
+            //   key={index}
+            // >
+            //   <h1 className="label-text text-black font-bold text-2xl pb-2">
+            //     {animal.name}
+            //   </h1>
+            //   <h2 className="label-text text-black font-bold text-sm">
+            //     Where do they live?
+            //   </h2>
+            //   {animal.locations.map((place, index) => {
+            //     return (
+            //       <p className="label-text text-black text-sm" key={index}>
+            //         {place}
+            //       </p>
+            //     );
+            //   })}
+            //   <h2 className="label-text text-black font-bold text-sm pt-2">
+            //     What do they eat?
+            //   </h2>
+            //   <p className="label-text text-black text-sm pb-2">
+            //     {animal.characteristics.prey}
+            //     {animal.characteristics.main_prey}
+            //   </p>
+            // </div>
           );
         })}
       </>
