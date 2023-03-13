5ed28b5cffc6e62211d312f4d58ec492ab7987e0
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-13 14:10:30 +0000

    Passed animalSightings controller GET test to return every sighting in collection

--- api/routes/animalSightings.js
+++ api/routes/animalSightings.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AnimalSightingsController = require("../controllers/animalSightings");
 
+router.get("/", AnimalSightingsController.Index);
 router.post("/", AnimalSightingsController.Create);
 
 module.exports = router;
