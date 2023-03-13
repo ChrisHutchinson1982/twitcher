3d6304c3a77fc46660848506d8d99ac50aa0a06e
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-13 10:27:10 +0000

    Passed searchAnimal unit test for render component with Animal name, location and main_prey

--- frontend/src/components/searchAnimal/searchAnimal.cy.js
+++ frontend/src/components/searchAnimal/searchAnimal.cy.js
@@ -2,7 +2,7 @@ import SearchAnimal from "./searchAnimal";
 import mockSearchResults from "../searchResults/mockSearchResults";
 
 describe("SearchAnimal", () => {
-  it("Rendres component with Animal name, location and prey", () => {
+  it("Renders component with Animal name, location and prey", () => {
     cy.mount(<SearchAnimal animal={mockSearchResults[0]} index={0} />);
 
     cy.get('[data-cy="animalNameResultAmerican Robin"]').should(
@@ -12,6 +12,22 @@ describe("SearchAnimal", () => {
       "Europe",
       "North-America",
       "earthworms, caterpillars, grasshoppers, beetle grubs, spiders, and snails"
+    );
+  });
+  it("Renders component with Animal name, location and main_prey", () => {
+    cy.mount(<SearchAnimal animal={mockSearchResults[2]} index={2} />);
+
+    cy.get('[data-cy="animalNameResultRobin"]').should(
+      "contain.text",
+      "Robin",
+      "Central-America",
+      "Europe",
+      "North-America",
+      "Africa",
+      "Asia",
+      "Eurasia",
+      "Europe",
+      "Worms, Insects, Fruit, Berries"
     );
   });
 });
