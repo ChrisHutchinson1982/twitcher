2337d466e8dde7f970a491d03256cc5b3e79a900
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-15 13:18:29 +0000

    Added CSS

--- frontend/src/components/sightingsLog/sightingsLog.js
+++ frontend/src/components/sightingsLog/sightingsLog.js
@@ -26,7 +26,7 @@ const SightingsLog = ({ sightings, setSightings }) => {
             Twitching Log
           </span>
         </label>
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-3 gap-4">
           {sightings.map((animal, index) => {
             return (
               <div key={index}>
