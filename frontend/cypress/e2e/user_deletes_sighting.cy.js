e689a6ae81ef8d035f9c1b44d6cf1202887d95ba
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-15 11:24:19 +0000

    Passing e2e test so user can sucessfully completes a search, saves and delete animal from twitching log

---
+++ frontend/cypress/e2e/user_deletes_sighting.cy.js
@@ -0,0 +1,29 @@
+import mockSearchResults from "../../src/components/searchResults/mockSearchResults";
+
+describe("User saves sightings", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "/animals?name=Robin", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: mockSearchResults,
+      });
+    }).as("getAnimals");
+  });
+
+  it("user sucessfully completes a search, saves and delets animal from twitching log", () => {
+    cy.visit("/");
+
+    cy.get('[data-cy="animalName"]').type("Robin");
+    cy.get('[data-cy="searchSubmit"]').click();
+
+    cy.wait("@getAnimals").then(() => {
+      cy.get('[data-cy="saveButton"]').first().click();
+      cy.get('[data-cy="animalLogAmerican Robin"]').should(
+        "contain.text",
+        "American Robin"
+      );
+      cy.get('[data-cy="deleteButton"]').last().click();
+      cy.get('[data-cy="animalLogAmerican Robin"]').should("not.exist");
+    });
+  });
+});
